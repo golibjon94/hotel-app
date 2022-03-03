b4f2f918c946ab17aa343f1584ef3e892cca5765
golibjon94 <golib94jon@gmail.com>
2022-03-03 18:33:58 +0500

    I started the hotel site project and prepared the header section.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './header.css';
+function Main() {
+  return (
+    <div>
+    <header>
+        <a href="#" class="brand">Amazing Hotel</a>
+        <div class="menu-btn"></div>
+        <div class="navigation">
+            <a href="#main">Home</a>
+            <a href="#about">About</a>
+            <a href="#skills">Skills</a>
+            <a href="#services">Services</a>
+            <a href="#work">Work</a>
+            <a href="#contact">Contact</a>
+        </div>
+    </header>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './main.css'
+function Main() {
+  return (
+    <div>
+
+<section class="main" id="main">
+      <div class="content">
+        <h2>
+          Welcome to <br />
+          <span>Amazing Hotel</span>
+        </h2>
+        <div class="animated-text">
+          <h3>A Best Place To Stay</h3>
+          <h3>The Best Dishes</h3>
+          <h3>Excellent Рotel Service</h3>
+        </div>
+        <a href="#" class="btn">See My Work</a>
+      
+      </div>
+    </section>
+
+    </div>
+  )
+}
+
+export default Main
